6471ffc0c3924c4e365ccd9b3bce77ed4096b4a2
Dominic Lees <dxl184@student.bham.ac.uk>
2023-11-11 15:58:07 +0000

    Created team schema

---
+++ schemas/team.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const teamSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    coach: {type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User'},
+    players: {type: [mongoose.Schema.Types.ObjectId], ref: 'User'},
+    code: {type: String, required: true}
+})
+
+module.exports = mongoose.model('Team', teamSchema);
