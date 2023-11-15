1b2dac068f603126ff33556e06e5ccc5900eb124
Dominic Lees <dxl184@student.bham.ac.uk>
2023-11-15 18:58:16 +0000

    Created game schema

--- schemas/game.js
+++ schemas/game.js
@@ -4,7 +4,7 @@ const gameSchema = new mongoose.Schema({
     team: {type: mongoose.Schema.Types.ObjectId, required: true, ref: 'Team'},
     opponent: {type: String, required: true},
     date: {type: Date, required: true},
-    playersSignedUp: {type: [mongoose.Schema.Types.ObjectId], required: true, ref: 'Team'},
+    playersSignedUp: {type: [mongoose.Schema.Types.ObjectId], required: true, ref: 'Player'},
 })
 
 module.exports = mongoose.model('Game', gameSchema);
