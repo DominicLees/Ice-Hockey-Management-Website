8b93e0c7a40d173e590cee586c53ba99aedcf5ac
Dominic Lees <dxl184@student.bham.ac.uk>
2023-11-15 14:25:43 +0000

    Corrected typo

--- routers/dashboard.js
+++ routers/dashboard.js
@@ -5,7 +5,7 @@ const Player = require('./../schemas/player');
 dashRouter.get('/', (req, res) => {
     Player.find({user: req.session.account._id}).populate('team').then(result => {
         res.render('dashboard', {
-            // Get list of team's user plays for from list of player profiles
+            // Get list of teams user plays for from list of player profiles
             teams: result.map(player => player.team) 
         })
     }).catch(error => {
