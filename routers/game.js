8216ee48de8b34812a38600d808784cf79553eed
Dominic Lees <dxl184@student.bham.ac.uk>
2023-11-16 16:28:52 +0000

    Added game creation

--- routers/game.js
+++ routers/game.js
@@ -1,9 +1,39 @@
 const express = require('express');
-const game = require('../schemas/game');
 const gameRouter = express.Router();
+const Game = require('../schemas/game');
 
 gameRouter.get('/new', (req, res) => {
     res.render('pages/game/new');
+})
+
+gameRouter.post('/new', (req, res) => {
+    // Validate Input
+    if (req.body.opponent.length == 0) {
+        req.session.responses.noOpponent = true;
+    } 
+    const inputDate = new Date(req.body.date);
+    if (isNaN(inputDate) || inputDate < new Date()) {
+        req.session.responses.pastDate = true;
+    }
+
+    if (Object.keys(req.session.responses).length > 0) {
+        return res.redirect(req.originalUrl);
+    }
+
+
+    // Add game to database
+    const newGame = new Game({
+        team: req.foundTeam._id,
+        opponent: req.body.opponent,
+        date: inputDate
+    })
+
+    newGame.save().then(result => {
+        res.redirect('/dashboard');
+    }).catch(error => {
+        console.log(error);
+        res.status(500).send();
+    })
 })
 
 module.exports = gameRouter;
--- schemas/game.js
+++ schemas/game.js
@@ -4,7 +4,7 @@ const gameSchema = new mongoose.Schema({
     team: {type: mongoose.Schema.Types.ObjectId, required: true, ref: 'Team'},
     opponent: {type: String, required: true},
     date: {type: Date, required: true},
-    playersSignedUp: {type: [mongoose.Schema.Types.ObjectId], required: true, ref: 'Player'},
+    playersSignedUp: {type: [mongoose.Schema.Types.ObjectId], ref: 'Player'},
 })
 
 module.exports = mongoose.model('Game', gameSchema);
