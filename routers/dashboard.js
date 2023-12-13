f54c0ee1a73211d0ad71800aada9b6265c2348a6
Dominic Lees <dxl184@student.bham.ac.uk>
2023-12-13 22:11:04 +0000

    Fixed dashboard to show teams user coaches

--- routers/dashboard.js
+++ routers/dashboard.js
@@ -1,16 +1,25 @@
 const express = require('express');
 const dashRouter = express.Router();
 const Player = require('./../schemas/player');
+const Team = require('./../schemas/team');
 
 dashRouter.get('/', (req, res, next) => {
+    let teams;
+    // Get list of teams user plays for from list of player profiles
     Player.find({user: req.session.account._id}).populate('team').then(result => {
+        teams = result.map(player => player.team)
+        // Get list of teams the user is the coach for 
+        return Team.find({coach: req.session.account._id})
+    }).then(result => {
+        // Combine the 2 list of teams
+        teams.concat(result);
+        // Sort the lists alphabetically
+        teams.sort((a, b) => a.name - b.name);
         res.render('dashboard', {
-            // Get list of teams user plays for from list of player profiles
-            teams: result.map(player => player.team) 
+            teams: teams
         })
     }).catch(error => {
         next(error);
     })
 })
-
 module.exports = dashRouter;
