dc28679d4264fea08b00e1c233440fafe93ed2c1
Dominic Lees <dxl184@student.bham.ac.uk>
2023-12-14 12:54:55 +0000

    Fixed team list not showing all of user's teams

--- routers/dashboard.js
+++ routers/dashboard.js
@@ -9,10 +9,10 @@ dashRouter.get('/', (req, res, next) => {
     Player.find({user: req.session.account._id}).populate('team').then(result => {
         teams = result.map(player => player.team)
         // Get list of teams the user is the coach for 
-        return Team.find({coach: req.session.account._id})
+        return Team.find({coach: req.session.account._id}).lean()
     }).then(result => {
         // Combine the 2 list of teams
-        teams.concat(result);
+        teams = teams.concat(result);
         // Sort the lists alphabetically
         teams.sort((a, b) => a.name - b.name);
         res.render('dashboard', {
