b46ff046dcb5f3ea7f1b5445fd1fcf828013f90c
Dominic Lees <dxl184@student.bham.ac.uk>
2023-12-21 15:48:08 +0000

    Cleaned up dashboard.js

--- routers/dashboard.js
+++ routers/dashboard.js
@@ -6,22 +6,21 @@ const Team = require('./../schemas/team');
 dashRouter.get('/', (req, res, next) => {
     let teams;
     // Get list of teams user plays for from list of player profiles
-    Player.find({user: req.session.account._id}).populate('team').then(result => {
-        teams = result.map(player => player.team)
+    Player.find({user: req.session.account._id}).lean().populate('team').then(result => {
+        teams = result.map(player => player.team);
         // Get list of teams the user is the coach for 
-        return Team.find({coach: req.session.account._id}).lean()
+        return Team.find({coach: req.session.account._id}).lean();
     }).then(result => {
-        // Combine the 2 list of teams
+        // Combine the 2 lists of teams
         teams = teams.concat(result);
         // Remove duplicates
-        teams = teams.filter((v,i,a)=>a.findIndex(v2=>(v2.code===v.code))===i)
+        teams = teams.filter((v, i, a) => a.findIndex(v2 => (v2.code === v.code)) === i);
         // Sort the lists alphabetically
         teams.sort((a, b) => a.name - b.name);
-        res.render('dashboard', {
-            teams: teams
-        })
+        res.render('dashboard', {teams});
     }).catch(error => {
         next(error);
     })
 })
+
 module.exports = dashRouter;
