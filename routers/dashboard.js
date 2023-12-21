0cc11551ae02bcaa50df0d59e4dae4ca9eb1d252
Dominic Lees <dxl184@student.bham.ac.uk>
2023-12-21 21:05:52 +0000

    Added upcoming games to dashboard

--- routers/dashboard.js
+++ routers/dashboard.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dashRouter = express.Router();
 const Player = require('./../schemas/player');
 const Team = require('./../schemas/team');
+const Game = require('./../schemas/game');
 
 dashRouter.get('/', (req, res, next) => {
     let teams;
@@ -17,8 +18,14 @@ dashRouter.get('/', (req, res, next) => {
         teams = teams.filter((v,i,a)=>a.findIndex(v2=>(v2.code===v.code))===i)
         // Sort the lists alphabetically
         teams.sort((a, b) => a.name - b.name);
+        // Take the _ids from the teams and put them into an array
+        const teamIds = teams.map(x => x._id);
+        // Find all the future games for the teams the user is involved with
+        return Game.find({team: {$in: teamIds}, date: {$gt: new Date()}}).lean().populate('team');
+    }).then(result => {
         res.render('dashboard', {
-            teams: teams
+            teams: teams,
+            games: result
         })
     }).catch(error => {
         next(error);
