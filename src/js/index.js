3a36e79b42d79a86a7b298f4eabde0137b2db120
Dominic Lees <dxl184@student.bham.ac.uk>
2023-12-22 22:20:35 +0000

    Added email regex to index page validation

--- src/js/index.js
+++ src/js/index.js
@@ -3,15 +3,16 @@ nameInput.addEventListener('focusout', () => {
     nameInput.style.backgroundColor = nameInput.value.length > 0 ? 'lightgreen' : 'red';
 })
 
+const emailRegEx = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 function validateEmail(input) {
-    if (input.value.length == 0) {
-        input.style.backgroundColor = 'red';
-    } else {
+    if (emailRegEx.test(input.value)) {
         input.style.backgroundColor = 'lightgreen';
+    } else {
+        input.style.backgroundColor = 'red';
     }
 }
 
 const loginEmail = document.getElementById('loginEmail');
 loginEmail.addEventListener('focusout', () => validateEmail(loginEmail));
 const signupEmail = document.getElementById('signupEmail');
-document.getElementById('signupEmail').addEventListener('focusout', () => validateEmail(signupEmail));
+signupEmail.addEventListener('focusout', () => validateEmail(signupEmail));
