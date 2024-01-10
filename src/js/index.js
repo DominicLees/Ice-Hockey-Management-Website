0ff04682eb2f0f48a93b8ceb260d05f6bed2e531
Dominic Lees <dxl184@student.bham.ac.uk>
2024-01-10 18:26:55 +0000

    First implentation of WebAuthn

--- src/js/index.js
+++ src/js/index.js
@@ -3,16 +3,61 @@ nameInput.addEventListener('focusout', () => {
     nameInput.style.backgroundColor = nameInput.value.length > 0 ? 'lightgreen' : 'red';
 })
 
-const emailRegEx = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-function validateEmail(input) {
-    if (emailRegEx.test(input.value)) {
-        input.style.backgroundColor = 'lightgreen';
-    } else {
-        input.style.backgroundColor = 'red';
-    }
+if (window.PublicKeyCredential) {
+    console.log('webAuth supported!');
+} else {
+    console.log('webAuth not supported :(')
 }
 
-const loginEmail = document.getElementById('loginEmail');
-loginEmail.addEventListener('focusout', () => validateEmail(loginEmail));
-const signupEmail = document.getElementById('signupEmail');
-signupEmail.addEventListener('focusout', () => validateEmail(signupEmail));
+const pubKeyCredParams = [{
+      "type": "public-key",
+      "alg": -7
+    }, {
+      "type": "public-key",
+      "alg": -8
+    }, {
+      "type": "public-key",
+      "alg": -36
+    }, {
+      "type": "public-key",
+      "alg": -37
+    }, {
+      "type": "public-key",
+      "alg": -38
+    }, {
+      "type": "public-key",
+      "alg": -39
+    }, {
+      "type": "public-key",
+      "alg": -257
+    }, {
+      "type": "public-key",
+      "alg": -258
+    }, {
+      "type": "public-key",
+      "alg": -259
+}]
+
+const publicKeyCredentialCreationOptions = {
+    challenge: Uint8Array.from("UZSL85T9AFC", c => c.charCodeAt(0)),
+    rp: {
+        name: "Hockey",
+    },
+    user: {
+        id: Uint8Array.from("HUEFRDFVHJFV", c => c.charCodeAt(0)),
+        name: "lee@webauthn.guide",
+        displayName: "Lee",
+    },
+    pubKeyCredParams,
+    authenticatorSelection: {
+        authenticatorAttachment: "platform",
+    },
+    timeout: 60000,
+    attestation: "direct"
+};
+
+navigator.credentials.create({publicKey: publicKeyCredentialCreationOptions}).then(credentials => {
+    console.log(credentials);
+}).catch(error => {
+    console.error(error);
+});
