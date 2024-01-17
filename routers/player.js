8534379c1bb1fc4562f34e20cffd16447e0f7e42
Dominic Lees <dxl184@student.bham.ac.uk>
2024-01-17 17:46:43 +0000

    Added player profile route to player router

--- routers/player.js
+++ routers/player.js
@@ -1,4 +1,16 @@
 const express = require('express');
 const playerRouter = express.Router();
+const Player = require('./../schemas/player');
+
+playerRouter.get('/:id', (req, res, next) => {
+    Player.findById(req.params.id).populate('team').then(result => {
+        if (result == null) {
+            res.redirect('/404');
+        }
+        res.render('playerProfile', {player: result})
+    }).catch(error => {
+        next(error);
+    })
+})
 
 module.exports = playerRouter;
