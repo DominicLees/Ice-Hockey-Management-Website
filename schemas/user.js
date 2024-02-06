cc4703e550071d34830c529b5b5b0247bea96524
Dominic Lees <dxl184@student.bham.ac.uk>
2024-02-06 18:29:12 +0000

    Updated user schema to include public key data

--- schemas/user.js
+++ schemas/user.js
@@ -3,7 +3,14 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     email: {type: String, required: true},
     name: {type: String, required: true},
-    verified: {type: Boolean, default: false}
+    credentialId: String,
+    publicKey: {
+        1: Number,
+        3: Number,
+        neg1: Number,
+        neg2: String,
+        neg3: String,
+    }
 })
 
 module.exports = mongoose.model('User', userSchema);
