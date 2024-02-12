ead1947157aed12584a87514ac883843009c0d10
Dominic Lees <dxl184@student.bham.ac.uk>
2024-02-12 17:45:55 +0000

    Added generating teams

--- testing/scripts/generateTestingDB.js
+++ testing/scripts/generateTestingDB.js
@@ -1,21 +1,41 @@
 const mongoose = require('mongoose');
+const crypto = require('crypto');
 const argv = require('yargs').argv;
 const config = require('./../../config.json');
-const names = require('./../data/users.json').names;
 const User = require('./../../schemas/user');
+const Team = require('./../../schemas/team');
+
+const userNames = require('./../data/users.json').names;
+const teamData = require('./../data/teams.json');
+const teamNames = teamData.teamNames;
+const places = teamData.places;
 
 function generateDB() {
     let users = [];
     for (let i = 0; i < argv.users; i++) {
-        const firstName = names[Math.floor(Math.random() * names.length)];
+        const firstName = userNames[Math.floor(Math.random() * userNames.length)];
         users.push({
-            name: `${firstName} ${names[Math.floor(Math.random() * names.length)]}`,
+            name: `${firstName} ${userNames[Math.floor(Math.random() * userNames.length)]}`,
             email: `${firstName}@hockey.com`
         })
     }
+
     User.insertMany(users).then(result => {
         console.log(`${result.length} users inserted into DB`);
+        let teams = []
+        for (let i = 0; i < argv.teams; i++) {
+            teams.push({
+                name: `${places[Math.floor(Math.random() * places.length)]} ${teamNames[Math.floor(Math.random() * teamNames.length)]}`,
+                coach: result[Math.floor(Math.random() * result.length)]._id,
+                code: crypto.randomBytes(3).toString('hex')
+            })
+        }
+        return Team.insertMany(teams);
+    }).then(result => {
+        console.log(`${result.length} teams inserted into DB`);
         process.exit();
+    }).catch(error => {
+        console.log(error);
     })
 }
 
