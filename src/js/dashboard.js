a182fe05cc908ceadf24ed9e0c49f0160f0bbb52
Dominic Lees <dxl184@student.bham.ac.uk>
2024-02-13 17:57:35 +0000

    Made dashboard menu visibility toggleable and automatic based on screen size

--- src/js/dashboard.js
+++ src/js/dashboard.js
@@ -1,5 +1,36 @@
 const teamCode = document.getElementById('teamCode');
+const teamMenu = document.getElementById('teamMenu');
+const teamMenuButton = document.getElementById('teamMenuButton');
+
+function hideMenu() {
+    teamMenu.classList.add('hidden');
+    teamMenuButton.innerHTML = 'Show Menu';
+}
+
+function showMenu() {
+    teamMenu.classList.remove('hidden');
+    teamMenuButton.innerHTML = 'Hide Menu';
+}
+
+function windowResize() {
+    if (window.innerWidth <= 722) {
+        hideMenu();
+    } else {
+        showMenu();
+    }
+}
 
 document.getElementById('joinTeamButton').addEventListener('click', () => {
     window.location = 'team/join/' + teamCode.value;
 })
+
+teamMenuButton.addEventListener('click', () => {
+    if (teamMenu.classList.contains('hidden')) {
+        showMenu();
+    } else {
+        hideMenu();
+    }
+})
+
+window.addEventListener('resize', windowResize);
+windowResize();
