bb9012e45fb8fa631fe51913892188c41cd7eb57
Dominic Lees <dxl184@student.bham.ac.uk>
2024-02-14 21:58:43 +0000

    Updated user schema to support multiple credentials

--- schemas/user.js
+++ schemas/user.js
@@ -3,14 +3,16 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     email: {type: String, required: true},
     name: {type: String, required: true},
-    credentialId: Buffer,
-    publicKey: {
-        1: Number,
-        3: Number,
-        neg1: Number,
-        neg2: Buffer,
-        neg3: Buffer,
-    }
+    credentials: [{
+        credentialId: Buffer,
+        publicKey: {
+            1: Number,
+            3: Number,
+            neg1: Number,
+            neg2: Buffer,
+            neg3: Buffer
+        }
+    }]
 })
 
 module.exports = mongoose.model('User', userSchema);
