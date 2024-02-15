9095122dc6a94d0396587a4b8636490fd33d2fc3
Dominic Lees <dxl184@student.bham.ac.uk>
2024-02-15 22:03:55 +0000

    Added message to settings page that warns them about the auth code expiring

--- src/js/settings.js
+++ src/js/settings.js
@@ -7,7 +7,11 @@ async function newCode() {
         throw new Error('Unable to generate code');
     }
     const code = await response.text();
-    newCodeDisplay.innerHTML = `Your code: ${code}`;
+    newCodeDisplay.innerHTML = `Your code: <strong>${code}</strong><br>This code is valid for the next 5 minutes.`;
+
+    setTimeout(() => {
+        newCodeDisplay.innerHTML = 'You code has expired. Please generate a new one.';
+    }, 300000);
 }
 
 document.getElementById('newCodeButton').addEventListener('click', newCode);
