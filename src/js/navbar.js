f69a2004d74cea72520771167cbb5c55a1501272
Dominic Lees <dxl184@student.bham.ac.uk>
2024-03-21 17:54:04 +0000

    Added navbar for mobile devices

---
+++ src/js/navbar.js
@@ -0,0 +1,6 @@
+const nav = document.getElementById('nav');
+const navToggle = document.getElementById('nav-toggle');
+
+navToggle.addEventListener('click', () => {
+    nav.classList.toggle('hidden');
+})
