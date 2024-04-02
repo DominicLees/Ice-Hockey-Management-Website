aba9a52ea400d08547fb4d52391dfdcf3e046aa0
Dominic Lees <dxl184@student.bham.ac.uk>
2024-04-02 11:25:07 +0100

    Cleaned up error.js

--- routers/error.js
+++ routers/error.js
@@ -1,20 +1,19 @@
 const express = require('express');
 const errorRouter = express.Router();
+const forbiddenError = require('./../functions/forbiddenError');
 
 const config = require('./../config.json');
-const enviroment = process.env.NODE_ENV || config.enviroment || "dev";
+const enviroment = process.env.NODE_ENV || config.enviroment || 'dev';
 
 // Testing routes only accessible during development
 errorRouter.use('/test', (req, res, next) => {
-    if (enviroment == "dev") { return next(); }
+    if (enviroment == 'dev') return next();
     res.redirect('/404');
 });
 
 // Routes that always throw errors to test error handling
 errorRouter.get('/test/403', (req, res, next) => {
-    const err = new Error('Forbidden');
-    err.status = 403;
-    next(err);
+    next(forbiddenError());
 })
 
 errorRouter.get('/test/500', (req, res, next) => {
